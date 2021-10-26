dd3381eb859ebf123e0aec215aeab160e1365f7b
JamesWithCode <minseok.jeong.james@gmail.com>
2021-10-26 23:26:46 +0900

    일반 회원가입 / 로그인 완료

---
+++ src/main/java/com/security/practice/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,32 @@
+package com.security.practice.config.auth;
+
+import com.security.practice.model.User;
+import com.security.practice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//시큐리티 설정에서 loginProcessingUrl("/login");
+//login  요청이 오면 자동으로 UserDetailsService 타입으로 IoC 되어 있는 loadUserByUsername 함수가 실행
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    //시큐리티 session (내부 Authentication(내부 UserDetails)) =>  캡슐 구조
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("username :" + username);
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity!=null){
+            return new PrincipalDetails(userEntity);
+        }
+        return null;
+    }
+}
--- src/main/java/com/security/practice/controller/IndexController.java
+++ src/main/java/com/security/practice/controller/IndexController.java
@@ -1,15 +1,56 @@
 package com.security.practice.controller;
 
+import com.security.practice.model.User;
+import com.security.practice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"","/"})
     public String index(){
         //머스테치 기본 폴더  src/main/resources/
         //뷰 리졸버 설정 : template (prefix), .mustache(suffix)
         return "index";
+    }
+
+    @GetMapping("/user")
+    public  @ResponseBody String user(){
+        return "user";
+    }
+    @GetMapping("/admin")
+    public   String admin(){
+        return "admin";
+    }
+    @GetMapping("/manager")
+    public   String manager(){
+        return "manager";
+    }
+    @GetMapping("/loginForm")
+    public  String loginForm(){
+        return "loginForm";
+    }
+    @GetMapping("/joinForm")
+    public  String joinForm(){
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public  String joinProc(User user){
+        user.setRole("ROLE_USER");
+        String rawPassword=user.getPassword();
+        String encPassword=bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); //회원가입이 잘됨 -> but 비밀번호가 암호화
+        return "redirect:/loginForm";
+
     }
 }
